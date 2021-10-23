a7519921db6f0eaf962842747be0650e34bd9e01
MrGanesh <ganeshkulkarni689@gmail.com>
2021-10-23 21:14:02 +0530

    4 files modified, 1 added

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,24 @@
-export const Header = () => {
+import Image from 'next/image';
+
+const Header = () => {
   return (
-    <div>
-      <h1>This is header</h1>
-    </div>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5">
+      {/* Left */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <h1>AirBnB</h1>
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      {/* Middle */}
+      <div></div>
+      {/* Right */}
+      <div></div>
+    </header>
   );
 };
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   future: {},
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
